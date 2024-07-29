4d446403550ea7eb14eae6a9fbd826fdb7fe358f
ThishmiGunawardhana <121964919+ThishmiGunawardhana@users.noreply.github.com>
2024-07-29 10:46:30 -0700

    create navbar components

--- thish_dress/src/Components/Navbar/Navbar.jsx
+++ thish_dress/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import './Navbar.css'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <img src="" alt="" className='logo'/>
+        <ul>
+            <li>Home</li>
+            <li>Collection</li>
+            <li>Cart</li>
+        </ul>
+
+        <div className='seach-box'>
+            <input type="text" plaseholder='search'/>
+            <img src="" alt=""/>
+        </div>
+
+        <img src="" alt="" className='logo'/>
+     
+    </div>
+
+
+
   )
 }
