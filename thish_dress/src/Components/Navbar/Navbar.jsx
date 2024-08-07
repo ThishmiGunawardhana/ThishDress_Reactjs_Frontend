f152a2003bddef256d822711652acf00351f7259
Praveen Dilshan <rosapraveen1@gmail.com>
2024-08-08 02:13:41 +0530

    Add a Navbar for the website

--- thish_dress/src/Components/Navbar/Navbar.jsx
+++ thish_dress/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-import './Navbar.css'
+import React from 'react';
+import './Navbar.css';
+import logo from '../../assets/1.png';
+import cart_icon from '../../assets/shopping-cart.png'
 
-export default function Navbar() {
+const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+    <div className='navbar'>
+      <div className='nav-logo'>
+        <img src={logo} alt='' className='logo'/>
+      </div>
+      <ul className='nav-menu'>
+        <li>Home<hr /></li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt='' className="cart-icon" />
+        <div className='nav-cart-count'>0</div>
+      </div>
+
+    </div>
+  );
 }
 
+export default Navbar;
